8aa517ee1ca719f71fc79a624da8f102838be390
Alex Merecka <merecka@gmail.com>
2019-04-10 23:31:07 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,22 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
+
+  def add_song
+    @songs << Artist.songs
+  end
+
+  def save
+    @@all << self
+  end
+
+
 
 end
