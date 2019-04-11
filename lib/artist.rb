7562727957478cbca4c48c41a75c9e319557b98b
Alex Merecka <merecka@gmail.com>
2019-04-11 04:43:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,19 +24,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist)
-    check = nil
-    self.all.collect do |x|
-    #  binding.pry
-      if x.name == artist
-        check = artist
-        x
-      end
-    end
-  #  binding.pry
-    if check = nil
-      new_artist = Artist.new(artist)
-      new_artist
-    end
+    artist_check = self.all.find {|x| x.name == artist}
+    artist_check ? artist_check : Artist.new(artist).save.last
   end
 
 end
